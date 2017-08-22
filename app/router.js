1b81b402861683ebfe4755a353fb6a1fb14b1228
Dustin Morris <dwmorris.biz@outlook.com>
2017-08-22 09:02:15 -0400

    additional features

--- app/router.js
+++ app/router.js
@@ -10,6 +10,10 @@ Router.map(function() {
   this.route('about');
   this.route('contact');
   this.route('portfolio');
+
+  this.route('admin', function() {
+    this.route('invitations');
+  });
 });
 
 export default Router;
